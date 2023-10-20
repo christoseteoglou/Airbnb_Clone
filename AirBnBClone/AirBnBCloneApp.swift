679932deaea781ad6aee73db619403e33a154087
christoseteoglou <itsmeglue@gmail.com>
2023-10-20 11:41:25 +0200

    Initial Commit

---
+++ AirBnBClone/AirBnBCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBCloneApp.swift
+//  AirBnBClone
+//
+//  Created by Christos Eteoglou on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnBClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirBnBClone
+//
+//  Created by Christos Eteoglou on 2023-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
